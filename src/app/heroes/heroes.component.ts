b75de39e3838ca8650b27f78cac4671831144643
Habib786 <habib-m-farooq@hotmail.com>
2022-03-26 21:29:58 +0300

    Added filter with route.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router'; // CLI imports router
+
 
 // Service.
 import { HeroesService } from '../heroes.service';
@@ -43,7 +46,7 @@ export class HeroesComponent implements OnInit {
     {value: 'tacos-2', viewValue: 'Tacos'},
   ];
 
-  constructor(private _heroesService: HeroesService, private _apiService: ApiService) { }
+  constructor(private _heroesService: HeroesService, private _apiService: ApiService, private location: Location, private router: Router) { }
 
   ngOnInit(): void {
     this._apiService.getCountries()
@@ -58,7 +61,53 @@ export class HeroesComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log('this.filterForm', this.filterForm);
+    console.log('this.filterForm', this.filterForm.value);
+
+    let configuration = [{
+      title: 'Email',
+      type: 'text',
+      value: this.filterForm?.value?.email || ''
+    },
+    {
+      title: 'Phone',
+      type: 'text',
+      value: this.filterForm?.value?.phone || ''
+    },
+    {
+      title: 'Name',
+      type: 'text',
+      value: this.filterForm?.value?.name || ''
+    },
+    {
+      title: 'Company',
+      type: 'text',
+      value: this.filterForm?.value?.company || ''
+    },
+   {
+      title: 'country',
+      type: 'dropdown',
+      api: 'http://countryapi.gear.host/v1/Country/getCountries?pLimit=25&pPage=1',
+      multiple: false,
+      value: this.filterForm?.value?.country || ''
+    },
+    {
+      title: 'Date',
+      type: 'date',
+      value: this.filterForm?.value?.date || ''
+    }];
+
+    this.router.navigate(
+      ['/heroes'],
+      { queryParams: {
+        name: this.filterForm?.value?.name || '',
+        phone: this.filterForm?.value?.phone || '',
+        email: this.filterForm?.value?.email || '',
+        date: this.filterForm?.value?.date ? this.filterForm.value.date.toISOString().slice(0, 10) : '',
+        country: this.filterForm?.value?.country || '',
+        company: this.filterForm?.value?.company || ''
+      } }
+    );
+
   }
 
 }
